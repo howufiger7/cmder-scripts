198c5d53bd1a2aaa1e2dbc07cfbdc0cb920ad5e4
Ilya Ayzenberg <howufiger7@gmail.com>
2015-04-14 17:39:19 -0400

    rpd - added -n option for new aweb - new alias for atlasweb

--- bin/rpd.bat
+++ bin/rpd.bat
@@ -1,6 +1,8 @@
 @echo off
 if ["%1"] == [""] (
 	start http://is.factset.com/rpd/
+) else if ["%1"] == ["-n"] (
+	start http://is.factset.com/rpd/createRPD.aspx
 ) else (
 	start http://is.factset.com/rpd/summary.aspx?messageId=%1
 )
